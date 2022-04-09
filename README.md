9b7cd6f6753e8253359f081dc7d7093afedc8c8b
Mauro Leggieri <mxmauro@gmail.com>
2022-04-09 12:09:16 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# github-app-installation-access-token
+# GitHub App Installation access token
+
 A NodeJS helper application to generate access tokens from GitHub apps and access private repositories.
+
+## Prerequisite. Create a custom GitHub application.
+
+1. Go to your account or organization settings.
+2. Go to `Developer settings`.
+3. Click on `New GitHub App`.
+4. Enter the application name and an optional description.
+   * Leave `Callback URL` empty.
+   * Turn on `Expire user authorization tokens` checkbox.
+   * Ensure to give Read-Only permissions to `Actions`, `Contents`, `Metadata`, `Packages` and `Secrets`. Adjust the rest on your needs.
+   * For the rest of the options, adjust to your needs.
+   * For organizations, may be convenient to enable subscription to events like `Push` and `Release`.
+   * In `Where can this GitHub App be installed?`, select `Only on this account`.
+5. Click on `Create GitHub App` to save the new app.
+6. Yoy will be forwarded to the application general settings edit page.
+7. Take note of the Application ID.
+8. Scroll to almost the bottom of the page and click on `Generate a private key`. Save the file on a safe place.
+9. Select `Install App` from the left menu and click on the Install App button next to your account/organization name.
+10. Select the repositories you want the app to access and click on the `Install` button.
+11. Once installed, you will be redirected to a page with an url like this `https://github.com/.../settings/installations/#####`.
+12. Take note of the number at the end. It is the Installation ID.
+
+## Using the NodeJS application.
+
+At this point you must have:
+
+1. The application ID.
+2. The installation ID.
+3. A file containing a RSA private key.
+
+### Generating a packed credentials token
+
+Run: `node cli.js get -a {APP-ID} -i {INSTALLATION-ID} -k "{PRIVATE-KEY}"`
+
+(to be continued)
+
+#### NOTE:
+The `-k, --privateKey` parameter accepts a file reference if you prefix the value with `@`.
+
+# License
+See `LICENSE` file for licensing details.
---
+++ cli.js
@@ -0,0 +1,135 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const { Command, Option } = require('commander');
+const { get, getWithToken } = require('./lib/access_token_retriever');
+const { generateToken } = require('./lib/generate_token');
+
+// ------------------------------------------------------------
+
+const program = new Command("github-app-installation-access-token");
+program.command('get')
+	.description('Get an installation token given an application id, installation id and private key')
+	.addOption(new Option('-a, --appId <app-id>', 'Application ID')
+		.env('GAIAT_APPID')
+		.makeOptionMandatory(true)
+		.argParser(parseAppId))
+	.addOption(new Option('-i, --installationId <installation-id>', 'Installation ID')
+		.env('GAIAT_INSTID')
+		.makeOptionMandatory(true)
+		.argParser(parseInstallationId))
+	.addOption(new Option('-k, --privateKey <pem>', 'Private key in PEM format')
+		.env('GAIAT_PKEY')
+		.makeOptionMandatory(true)
+		.argParser(parsePrivateKey))
+	.action(get);
+
+program.command('cred')
+	.description('Get an installation token given a base64 encoded credentials')
+	.addOption(new Option('-c, --credentials <credentials-token>', 'Base64 encoded credentials')
+		.env('GAIAT_TOKEN')
+		.makeOptionMandatory(true)
+		.argParser(parseCredentials))
+	.action(getWithToken);
+
+program.command('gen')
+	.description('Generates a base64 encoded credentials given an application id, installation id and private key')
+	.addOption(new Option('-a, --appId <app-id>', 'Application ID')
+		.env('GAIAT_APPID')
+		.makeOptionMandatory(true)
+		.argParser(parseAppId))
+	.addOption(new Option('-i, --installationId <installation-id>', 'Installation ID')
+		.env('GAIAT_INSTID')
+		.makeOptionMandatory(true)
+		.argParser(parseInstallationId))
+	.addOption(new Option('-k, --privateKey <pem>', 'Private key in PEM format')
+		.env('GAIAT_PKEY')
+		.makeOptionMandatory(true)
+		.argParser(parsePrivateKey))
+	.action(generateToken);
+
+program.parse();
+
+// ------------------------------------------------------------
+
+function getAppId(value) {
+	if (typeof value !== 'string') {
+		return false;
+	}
+	if (!((/^[0-9]+$/ui).test(value))) {
+		return false;
+	}
+	return parseInt(value, 10);
+}
+
+function parseAppId(value) {
+	value = getAppId(value);
+	if (value === false) {
+		program.error('invalid application id argument');
+	}
+	return value;
+}
+
+function getInstallationId(value) {
+	if (typeof value !== 'string') {
+		return false;
+	}
+	if (!((/^[0-9]+$/ui).test(value))) {
+		return false;
+	}
+	return parseInt(value, 10);
+}
+
+function parseInstallationId(value) {
+	value = getInstallationId(value);
+	if (value === false) {
+		program.error('invalid installation id argument');
+	}
+	return value;
+}
+
+function getPrivateKey(value) {
+	if (typeof value !== 'string') {
+		return false;
+	}
+	if (value.startsWith('@')) {
+		try {
+			value = fs.readFileSync(value.substring(1), 'utf8');
+		}
+		catch (err) {
+			return false;
+		}
+	}
+	const startIdx = value.indexOf('-----BEGIN RSA PRIVATE KEY-----');
+	const endIdx = value.indexOf('-----END RSA PRIVATE KEY-----');
+	if (startIdx < 0 || endIdx < 0 || endIdx <= startIdx) {
+		return false;
+	}
+	return value;
+}
+
+function parsePrivateKey(value) {
+	value = getPrivateKey(value);
+	if (value === false) {
+		program.error('invalid private key argument');
+	}
+	return value;
+}
+
+function parseCredentials(value) {
+	if (!value) {
+		program.error('invalid credentials argument');
+	}
+	try {
+		const payload = JSON.parse(Buffer.from(value, 'base64').toString());
+		if (getAppId(payload.appId) === false || getInstallationId(payload.installationId) === false) {
+			throw new Error("invalid payload");
+		}
+		if (typeof payload.privateKey !== 'string' || payload.privateKey.startsWith('@') || getPrivateKey(payload.privateKey) === false) {
+			throw new Error("invalid payload");
+		}
+		return payload;
+	}
+	catch (err) {
+		program.error('invalid credentials argument');
+	}
+}
